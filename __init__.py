0a48f44fb68ceb6f6bad36cb38c21f38bfd0f02b
MiddleKD <kmiddlecenter@gmail.com>
2024-07-31 11:59:50 +0900

    first commit

---
+++ __init__.py
@@ -0,0 +1,10 @@
+import sys, os
+sys.path.append(os.path.dirname(__file__))
+from .node import (ResetModelPatcherCalculateWeight)
+
+NODE_CLASS_MAPPINGS = {
+}
+NODE_DISPLAY_NAME_MAPPINGS = {
+}
+
+__all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
